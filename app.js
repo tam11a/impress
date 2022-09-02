97988d17172d2d155eaa87552661f894878b2645
tam11a <ibrahimsadiktamim@gmail.com>
2022-09-03 01:33:52 +0600

    added: cors with app.use

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const swaggerOptions = {
 
 // Use Cors
 const corsConfig = cors({
-  origin: ["http://localhost:3000"],
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 200,
@@ -40,9 +40,11 @@ const corsConfig = cors({
 // Use Routes
 app.use(express.json());
 
-app.use("/api/device", corsConfig, require("./routes/device"));
-app.use("/api/state", corsConfig, require("./routes/state"));
-app.use("/api/alert", corsConfig, require("./routes/alert"));
+app.use(corsConfig);
+
+app.use("/api/device", require("./routes/device"));
+app.use("/api/state", require("./routes/state"));
+app.use("/api/alert", require("./routes/alert"));
 
 // Add Swagger UI to Home Page
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerOptions)));
